a1f3e4a96932d67c3ab74c0188e7c1f4b6b3d6b7
Hoybaby <mbartek436@gmail.com>
2021-05-17 23:30:24 -0400

    having a lesson to show how to link nodes together with a method

--- Data Structures/Nodes/simple-node.js
+++ Data Structures/Nodes/simple-node.js
@@ -7,11 +7,28 @@ class Node {
         this.next = null;
     }
 
+    // we want to allow the node to be updated so it can be traversed and it can do so with this method.
+    setNextNode(node) {
+        this.next = node;
+    }
+
 }
+
+
+
 
 const firstNode = new Node(5);
 
 console.log(firstNode.data);
 console.log(firstNode.next);
+
+// Updating it 
+
+const secondNode = new Node();
+// with this the next is updated
+firstNode.setNextNode(secondNode);
+
+console.log(firstNode) // this will show the first node then in the next: it will reveal the next node
+
 
 module.exports = Node;
