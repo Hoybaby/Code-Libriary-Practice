15b8666aaf0474e6c251fc6ff73810a72b38643c
Hoybaby <mbartek436@gmail.com>
2021-06-03 13:18:54 -0400

    adding more functions to my sql database knowlesge

---
+++ sql/aggregate functions.sql
@@ -0,0 +1,5 @@
+-- it is possible to count how many rows are in a table in case there is alot, 
+-- to do so you can use this function below. table name is w.e it might be
+
+
+Select Count(*) from table_name; 
--- sql/other.sql
+++ sql/other.sql
@@ -9,3 +9,19 @@ SELECT * from movies where name like 'The %';
 
 -- Can look for null with this
 SELECT NAME from movies where imdb_rating IS NULL;
+
+-- one can query a table and order it in one with this command
+-- with key words like DESC and ASC you can choose how
+select name,year 
+from movies 
+order by name;
+
+-- another example from above
+select name, year, imdb_rating
+from movies 
+order by imdb_rating DESC ;
+
+-- this will show the best reviews as high
+select * from nomnom
+WHERE review
+ORDER BY review DESC;
